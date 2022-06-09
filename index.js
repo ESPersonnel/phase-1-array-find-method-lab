f0ef221b031c9b5726b7fdebcd9030b9f73757a8
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-09 15:13:47 +0300

    commit tree 125a56e948316fe3d52e4072f35e7f2a52c4f9bf

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+// Write a function called superbowlWin() in index.js:
+
+// The function should receive 1 argument, an Array of JavaScript Objects
+// Each object has two properties: year and result
+// It should use find() to test each Object to see if the result is "W" — a win!
+// It should return the year when the win occurred (if it occurred at all!)
+// If no win is found, it should return undefined
+
+// function superbowlWin(arr) {
+//   return arr.find(obj => obj.result === "W").year;
+// }
+
+// repeat the function and return undefined if no win is found
+
+function superbowlWin(arr) {
+  return arr.find(obj => obj.result === "W") ? arr.find(obj => obj.result === "W").year : undefined;
+}
